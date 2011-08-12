a1cdca563c03282665e1faa7b97ca69cdc52e259
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2011-08-12 11:13:26 -0400

    work on making Ticket objects take tickets as input

--- pubtkt/ticket.py
+++ pubtkt/ticket.py
@@ -11,11 +11,13 @@ class TicketError(Exception):
 class MissingFieldError (TicketError):
     pass
 
+def split_token(tok):
+    return dict([x.split('=',1) for x in tok.split(';')])
+
 class Ticket (dict):
 
-    def __init__(self, privkey, *args, **kwargs):
+    def __init__(self, ticket=None, *args, **kwargs):
         super(Ticket, self).__init__(*args, **kwargs)
-        self.privkey = privkey
 
         self.fields = [
                 ('uid',	        str,	        True),
@@ -25,7 +27,21 @@ class Ticket (dict):
                                                 False),
                 ('udata',	    str,	        False),
                 ('graceperiod',	self.parsedate,	False),
+                ('sig',         str,            False),
                 ]
+
+        if ticket is not None:
+            if isinstance(ticket, dict):
+                self.from_dict(ticket)
+            else:
+                self.from_string(ticket)
+
+    def from_dict (self, ticket):
+        self.update(ticket)
+
+    def from_string (self, ticket):
+        self.update(split_token(ticket))
+        self['tokens'] = self.get('tokens', '').split(',')
 
     def parsedate(self, d):
         if isinstance(d, datetime.timedelta):
@@ -38,6 +54,7 @@ class Ticket (dict):
 
     def ticket (self):
         dtok = []
+
         for fspec in self.fields:
             if fspec[2] and not fspec[0] in self:
                 raise MissingFieldError('missing required field: %s' %
@@ -47,20 +64,24 @@ class Ticket (dict):
                     fspec[1](self[fspec[0]])))
 
         stok = ';'.join(dtok)
+        return stok
+
+    def sign (self, privkey):
+        if isinstance(privkey, str):
+            privkey = RSA.load_key(privkey)
+
+        stok = self.ticket()
+
         dgst = hashlib.sha1(stok).digest()
-        sig = self.privkey.sign(dgst, 'sha1')
+        sig = privkey.sign(dgst, 'sha1')
         sig = base64.b64encode(sig)
 
-        return '%s;sig=%s' % (stok, sig)
+        self['sig'] = sig
 
     def __str__ (self):
         return self.ticket()
 
 if __name__ == '__main__':
-    k = RSA.load_key('privkey.pem')
-    p = Ticket(k, uid='lars',
-            validuntil = datetime.timedelta(minutes=10),
-            graceperiod = datetime.timedelta(minutes=5))
-
-    print p
+    t1 = Ticket(uid='lars', validuntil=datetime.timedelta(hours=1),
+            tokens=['one', 'two', 'three'])
 
